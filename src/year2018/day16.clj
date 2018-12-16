4fea03982d0874cdaf1676ba9cd896f18a6ee6da
Hyunwoo Nam <hw.nam@ridi.com>
2018-12-16 18:11:09 +0900

    day16

--- src/year2018/day16.clj
+++ src/year2018/day16.clj
@@ -1,5 +1,6 @@
 (ns year2018.day16
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]))
 
 
 (defn machine [registers]
@@ -11,7 +12,8 @@
 (defn store [m r v]
   (update m :registers assoc r v))
 
-(def opcodes [:addr :addi :mulr :muli :banr :bani :borr :bori :setr :seti :gtir :gtri :gtrr :eqir :eqri :eqrr])
+(def opcodes #{:addr :addi :mulr :muli :banr :bani :borr :bori :setr :seti :gtir :gtri :gtrr :eqir :eqri :eqrr})
+(set/intersection (set opcodes) [:addr :muli])
 
 (defn run-inst [m [op in1 in2 out]]
   (case op
@@ -55,23 +57,63 @@
     (store m out (if (= (load m in1) (load m in2)) 1 0))
     ))
 
-(defn num-candidates [[before [_ in1 in2 out] after]]
-  (->> (map #(run-inst before [% in1 in2 out]) opcodes)
-       (filter #(= (:registers after) (:registers %)))
-       (count)))
-
-(def input (-> "day16-1.in" io/resource io/reader line-seq))
+(defn sample->candidates [[before [op in1 in2 out] after]]
+  (let [candidates (filter #(= (:registers (run-inst before [% in1 in2 out]))
+                               (:registers after))
+                           opcodes)]
+    [op candidates]))
 
 (defn parse-sample [[before inst after]]
   [(machine (read-string (subs before 8)))
    (read-string (str "[" inst "]"))
    (machine (read-string (subs after 8)))])
 
-@(def samples
-  (->> input
-       (remove #(zero? (count %)))
-       (partition 3)
-       (map parse-sample)
-       (map num-candidates)
-       (filter #(>= % 3))
-       (count)))
+(def samples (->> (-> "day16-1.in" io/resource io/reader line-seq)
+                  (remove #(zero? (count %)))
+                  (partition 3)
+                  (map parse-sample)))
+
+@(def part1
+   (->> samples
+        (map sample->candidates)
+        (filter #(>= (count (second %)) 3))
+        (count)))
+
+
+(def opcode-map {0  :eqir
+                 1  :seti
+                 2  :eqri
+                 3  :eqrr
+                 4  :addi
+                 5  :setr
+                 6  :gtrr
+                 7  :gtri
+                 8  :muli
+                 9  :bori
+                 10 :bani
+                 11 :borr
+                 12 :gtir
+                 13 :banr
+                 14 :addr
+                 15 :mulr})
+
+(def init-map (zipmap (range 16) (repeat opcodes)))
+
+(->> samples
+     (map sample->candidates)
+     (reduce (fn [res [op candidates]]
+               (update res op set/intersection (set candidates)))
+             init-map)
+     ; manually repeat eval with updated opcode-map
+     (map (fn [[op candidates]]
+            [op (set/difference candidates (set (vals opcode-map)))]))
+     (remove #(empty? (second %))))
+
+@(def instructions (->> (-> "day16-2.in" io/resource io/reader line-seq)
+                        (map #(read-string (str "[" % "]")))))
+
+(def part2 (reduce (fn [m [op & args]]
+                     (run-inst m (cons (opcode-map op) args)))
+                   (machine [0 0 0 0])
+                   instructions
+                   ))
