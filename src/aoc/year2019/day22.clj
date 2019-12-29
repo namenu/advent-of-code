a37126b06e10998747b4f0048a41ed2eb610f3fb
Hyunwoo Nam <hw.nam@ridi.com>
2019-12-29 12:40:22 +0900

    solve with LCG

--- src/aoc/year2019/day22.clj
+++ src/aoc/year2019/day22.clj
@@ -13,31 +13,33 @@
 ; 건너뛰기 ax + b, n -> anx + nb
 ; 시작 상태는 0~D-1까지 정렬된 상태라고 했으므로, a=1, b=0으로 초항이 결정된다.
 
-(defn deal-stack-of [[a b]]
+(defn deal-stack [[a b]]
   [(- a), (- (- b) 1)])
 
-(defn cut-deck-of [n [a b]]
-  [a, (mod (- b n) D)])
+(defn cut-deck-of [n]
+  (fn [[a b]]
+    [a, (mod (- b n) D)]))
 
-(defn deal-inc-of [n [a b]]
-  [(mod (* a n) D), (mod (* b n) D)])
+(defn deal-inc-of [n]
+  (fn [[a b]]
+    [(mod (* a n) D), (mod (* b n) D)]))
 
-(defn solve [[a b] x]
-  (mod (+ (* a x) b) D))
+(defn lcg-next [[a b] [ax bx]]
+  [(mod (* a ax) D), (mod (+ (* a bx) b) D)])
 
 (defn parse-line [line stack-fn cut-fn inc-fn]
   (let [[a b _ d] (str/split line #" ")]
     (if (= a "cut")
-      (partial cut-fn (Long/parseLong b))
+      (cut-fn (Long/parseLong b))
       (if (= b "with")
-        (partial inc-fn (Long/parseLong d))
+        (inc-fn (Long/parseLong d))
         stack-fn))))
 
 ; pt.1
 (binding [D 10007]
-  (let [shuffles (map #(parse-line % deal-stack-of cut-deck-of deal-inc-of) (input-lines 2019 22))
+  (let [shuffles (map #(parse-line % deal-stack cut-deck-of deal-inc-of) (input-lines 2019 22))
         F        (apply comp (reverse shuffles))]
-    (solve (F [1 0]) 2019)))
+    (lcg-next (F [1 0]) [0 2019])))
 
 
 ; pt.2
@@ -49,10 +51,9 @@
 ; 건너뛰기 h(x)의 역은 h(modInv(n))
 ; modInv는 내장함수를 이용하였음.
 ;
-; (F^-1)^R 에서 R은 매우 큰 값이 나오는데, F^-1도 1차 다항식의 모노이드 연산임을 고려할 때
+; (F^-1)^R 에서 R은 매우 큰 값이 나오는데, F^-1도 LCG에 대한 호모몰피즘임을 고려할 때
 ; (ax+b)^n = a^nx + b(a^n-1)(a-1)^1
 ; 형태로 단순화 할 수 있으며, 빠른 지수승 계산에는 modPow내장함수를 이용하였음.
-
 
 (defn mod-inv [x]
   (.modInverse (biginteger x) (biginteger D)))
@@ -60,18 +61,18 @@
 (defn mod-exp [x n]
   (.modPow (biginteger x) (biginteger n) (biginteger D)))
 
-(def inv-deal-stack-of
-  deal-stack-of)
+(def inv-deal-stack
+  deal-stack)
 
-(defn inv-cut-deck-of [n [a b]]
-  (cut-deck-of (- n) [a b]))
+(defn inv-cut-deck-of [n]
+  (cut-deck-of (- n)))
 
-(defn inv-deal-inc-of [n [a b]]
-  (deal-inc-of (mod-inv n) [a b]))
+(defn inv-deal-inc-of [n]
+  (deal-inc-of (mod-inv n)))
 
-(defn repeat-of [n [a b]]
+(defn lcg-nth [n [a b]]
   [(mod-exp a n), (mod (* b (* (- (mod-exp a n) 1) (mod-inv (- a 1)))) D)])
 
-(let [shuffles (map #(parse-line % inv-deal-stack-of inv-cut-deck-of inv-deal-inc-of) (input-lines 2019 22))
+(let [shuffles (map #(parse-line % inv-deal-stack inv-cut-deck-of inv-deal-inc-of) (input-lines 2019 22))
       inv-F    (apply comp shuffles)]
-  (solve (repeat-of R (inv-F [1 0])) 2020))
+  (lcg-next (lcg-nth R (inv-F [1 0])) [0 2020]))
