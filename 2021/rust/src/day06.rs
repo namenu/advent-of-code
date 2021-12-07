3e12f01a87b6d6e4be0ee135950ad7261bf37bce
Hyunwoo Nam <namenu@gmail.com>
2021-12-08 03:35:22 +0900

    d6 - O(n)

--- 2021/rust/src/day06.rs
+++ 2021/rust/src/day06.rs
@@ -1,18 +1,14 @@
-use std::fs;
+use std::collections::HashMap;
 
-static SAMPLE: &str = "16,1,2,0,4,2,7,1,2,14";
-
-fn parse_input() -> Vec<i64> {
-    let s = fs::read_to_string("inputs/06.in").unwrap();
-    let s = SAMPLE;
+fn parse_input(s: &str) -> Vec<i32> {
     s.split(",").flat_map(|n| n.parse()).collect()
 }
 
-fn cost(nums: &Vec<i64>, pos: i64) -> i64 {
+fn cost(nums: &Vec<i32>, pos: i32) -> i32 {
     nums.iter().map(|&x| (pos - x).abs()).sum()
 }
 
-fn cost2(nums: &Vec<i64>, pos: i64) -> i64 {
+fn cost2(nums: &Vec<i32>, pos: i32) -> i32 {
     nums.iter()
         .map(|&x| {
             let l = (pos - x).abs();
@@ -21,41 +17,78 @@ fn cost2(nums: &Vec<i64>, pos: i64) -> i64 {
         .sum()
 }
 
-pub fn part1() -> i64 {
-    let nums = parse_input();
+type CostFn = fn(&Vec<i32>, i32) -> i32;
 
+fn find_min_fuel(nums: &Vec<i32>, f: CostFn) -> i32 {
     let min = *nums.iter().min().unwrap();
     let max = *nums.iter().max().unwrap() + 1;
-
-    let v = (min..max).map(|x| cost(&nums, x)).min().unwrap();
-    println!("{:?}", v);
-    
-    v
+    (min..max).map(|x| f(&nums, x)).min().unwrap()
 }
 
-pub fn part2() -> i64 {
-    let lines = parse_input();
-    
-    let nums = parse_input();
-    
-    let min = *nums.iter().min().unwrap();
-    let max = *nums.iter().max().unwrap() + 1;
-    
-    let v = (min..max).map(|x| cost2(&nums, x)).min().unwrap();
-    println!("{:?}", v);
+pub fn part1(s: &str) -> i32 {
+    let nums = parse_input(s);
 
-    v
+    // TODO: cost 를 closure 로 선언하고 싶음
+    find_min_fuel(&nums, cost)
+}
+
+pub fn part1_linear(s: &str) -> i32 {
+    let nums = parse_input(s);
+
+    let mut h: HashMap<i32, i32> = HashMap::new();
+    let mut lhs = 0;
+    let mut rhs = 0;
+    let mut fuel = 0;
+
+    for &n in &nums {
+        let e = h.entry(n).or_insert(0);
+        *e += 1;
+        fuel += n + 1;
+        rhs += 1;
+    }
+
+    let max = *nums.iter().max().unwrap() + 1;
+    let mut min_fuel = fuel;
+
+    for pos in 0..max {
+        if let Some(v) = h.remove(&pos) {
+            rhs -= v;
+            fuel += lhs - rhs - v;
+            lhs += v;
+        } else {
+            fuel += lhs - rhs;
+        }
+        min_fuel = min_fuel.min(fuel);
+    }
+
+    min_fuel
+}
+
+pub fn part2(s: &str) -> i32 {
+    let nums = parse_input(s);
+    find_min_fuel(&nums, cost2)
 }
 
 #[cfg(test)]
 mod tests {
+    static SAMPLE: &str = "16,1,2,0,4,2,7,1,2,14";
+    static INPUT: &str = include_str!("../inputs/06.in");
+
     #[test]
     fn part1() {
-        assert_eq!(super::part1(), 347509)
+        assert_eq!(super::part1(SAMPLE), 37);
+        assert_eq!(super::part1(INPUT), 347509);
+    }
+
+    #[test]
+    fn part1_linear() {
+        assert_eq!(super::part1_linear(SAMPLE), 37);
+        assert_eq!(super::part1_linear(INPUT), 347509);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(), 98257206)
+        assert_eq!(super::part2(SAMPLE), 168);
+        assert_eq!(super::part2(INPUT), 98257206);
     }
 }
