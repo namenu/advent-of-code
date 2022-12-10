f8472fb8c49a8c48bd213bc2ddaa1c2c4f537e9f
Hyunwoo Nam <namenu@gmail.com>
2022-12-10 14:41:54 +0900

    y2022-day9

--- 2022/clojure/src/aoc/year2022/day09.clj
+++ 2022/clojure/src/aoc/year2022/day09.clj
@@ -4,7 +4,15 @@
 (def input "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2")
 
 (def state0 {:head  [0 0]
-             :tail  [0 0]})
+             :tail1  [0 0]
+             :tail2  [0 0]
+             :tail3  [0 0]
+             :tail4  [0 0]
+             :tail5  [0 0]
+             :tail6  [0 0]
+             :tail7  [0 0]
+             :tail8  [0 0]
+             :tail9  [0 0]})
 
 (defn move-dir [pos dir]
   (mapv + pos (case dir
@@ -29,10 +37,27 @@
               (> dy 1) (move-dir "U")
               (< dy -1) (move-dir "D")))))
 
-(defn move [{:keys [head tail] :as state} d]
+(defn move [{:keys [head tail1 tail2 tail3 tail4 tail5 tail6 tail7 tail8 tail9] :as state} d]
   (let [head (move-dir head d)
-        tail (move-toward tail head)]
-    (assoc state :head head :tail tail)))
+        tail1 (move-toward tail1 head)
+        tail2 (move-toward tail2 tail1)
+        tail3 (move-toward tail3 tail2)
+        tail4 (move-toward tail4 tail3)
+        tail5 (move-toward tail5 tail4)
+        tail6 (move-toward tail6 tail5)
+        tail7 (move-toward tail7 tail6)
+        tail8 (move-toward tail8 tail7)
+        tail9 (move-toward tail9 tail8)]
+    (assoc state :head head
+                 :tail1 tail1
+                 :tail2 tail2
+                 :tail3 tail3
+                 :tail4 tail4
+                 :tail5 tail5
+                 :tail6 tail6
+                 :tail7 tail7
+                 :tail8 tail8
+                 :tail9 tail9)))
 
 (defn parse-input [input]
   (->> (str/split-lines input)
@@ -40,10 +65,14 @@
        (map #(update % 1 parse-long))))
 
 (def input (slurp (clojure.java.io/resource "day09.in")))
+
+(def input "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20")
+
+;; part2
 (let [moveseq (->> (parse-input input)
                    (mapcat (fn [[dir times]] (repeat times dir))))]
   (->> (reductions move state0 moveseq)
-       (map :tail)
+       (map :tail9)
        set
        count))
 
